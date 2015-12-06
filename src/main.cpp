9785f43da7c5e3060a16cb18a297aebd0f98c7e5
Kevin J Reece <kevinjreece@gmail.com>
2015-12-06 12:08:19 -0700

    added timing checks for each major section in main

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,19 @@ int main(int argc, char* argv[]) {
 	double startTime = getCurrentTime();
 
 	std::vector<std::string> imageNames = getImagesFromDirectory(argv[1]);
+	
+	double getImagesTime = getCurrentTime();
+	std::cout << "Get images time (s): " << getImagesTime - startTime << "\n";
+
 	std::vector<Image*> images(imageNames.size(), NULL);
 
-	#pragma omp parallel for
+	// #pragma omp parallel for
 	for (int i = 0; i < imageNames.size(); i++) {
 		images[i] = new Image(imageNames[i]);
 	}
+
+	double createImagesTime = getCurrentTime();
+	std::cout << "Create images time (s): " << createImagesTime - getImagesTime << "\n";
 
 	int mosaicHeight, mosaicWidth;
 	std::istringstream ss;
@@ -41,6 +48,7 @@ int main(int argc, char* argv[]) {
 		minWidth = (curWidth < minWidth) ? curWidth : minWidth;
 	}
 
+	// #pragma omp parallel for
 	for (int i = 0; i < images.size(); i++) {
 		images[i] = images[i]->crop(minHeight, minWidth);
 		// std::string oldName(stripFileExtension(getBaseName(imageNames[i])));
@@ -48,8 +56,18 @@ int main(int argc, char* argv[]) {
 		// images[i]->writeToFile(newFileName);
 	}
 
+	double cropImagesTime = getCurrentTime();
+	std::cout << "Crop images time (s): " << cropImagesTime-createImagesTime << "\n";
+
 	Image* mosaic = stitch(images, mosaicHeight, mosaicWidth);
+
+	double stitchTime = getCurrentTime();
+	std::cout << "Stitch time (s): " << stitchTime-cropImagesTime << "\n";
+
 	mosaic->writeToFile("output/mosaic.ppm");
+
+	double writeTime = getCurrentTime();
+	std::cout << "Write time (s): " << writeTime-stitchTime << "\n";
 
 	double endTime = getCurrentTime();
 	std::cout << "Execution time (s): " << endTime-startTime << "\n";
